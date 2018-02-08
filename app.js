79b3e8351ea3ee9a4d201c3cbc3bbc0a403762c9
Prab <prab.missier@gmail.com>
2018-02-08 19:52:47 +1100

    used Olsen tz

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const mytweet6 ={
 }
 
 var dayOfWeek = require('day-of-week').get
-dayOfWeek(new Date(2018, 1, 1), 'America/Los_Angeles');
+dayOfWeek(new Date(2018, 2, 8), 'Australia/Melbourne');
 
 console.log(dayOfWeek);
 
