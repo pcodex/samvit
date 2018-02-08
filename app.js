159906326312e3e43ca4801e6c9970954496c229
Prab <prab.missier@gmail.com>
2018-02-08 20:36:22 +1100

    Removed dead code

--- app.js
+++ app.js
@@ -64,38 +64,8 @@ T.post('statuses/update', mytweet, (err, response) => {
         if(err){
            return console.log('Post failed : '+err[0].message);
         }
-      const username = response.user.screen_name;
-      const followerscnt = response.friends_count;
-      //const favoritedTweetId = response.id_str;
-      console.log(`Your name:`+username);
-      console.log(`Your follower count:`+followerscnt);
-    });
-
-/*
-// Initiate your search using the above paramaters
-T.get('search/tweets', params, (err, data, response) => {
-  
-    // If there is no error, proceed
-    if(err){      
-      return console.log(err);
-    }  
-    console.log(data.statuses[0].id_str);
-  
-    const tweetsId = data.statuses[0].id_str;    
-    T.post('statuses/retweet/:id', tweetsId, (err, response) => {
-  
-        if(err){
-           return console.log('Retweet failed : '+err[0].message);
-        }
-      const username = response.user.screen_name;
-      //const favoritedTweetId = response.id_str;
-      console.log(`Retweeted: https://twitter.com/${username}`);
-    });
-  
-});
-*/
-
-
-      
+      const username = response.user.screen_name;      
+      console.log(`Your name:`+username);      
+    });   
   
 
