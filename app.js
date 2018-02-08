cfcde0394e6dbb1b22df5670df455b13aaf6c222
Prab <prab.missier@gmail.com>
2018-02-08 19:33:23 +1100

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,39 @@ const params = {
   lang: 'en'
 }*/
 
-const mytweet ={
+const mytweet0 ={
+  status:'The sun never sets at Samvit'  
+}
+
+const mytweet1 ={
+  status:'Working hard on the #UXDesign for a client website'  
+}
+
+const mytweet2 ={
+  status:'Getting into the nuts and bolts of #Multithreading with #cplusplus'  
+}
+
+const mytweet3 ={
+  status:'The Phoenix Project is a must read for all #Devops and #SoftwareEngineer at #Samvit'  
+}
+
+const mytweet4 ={
+  status:'The most elegant #UXdesign isthe simplest and most intuitive. Less is more at #Samvit'
+}
+
+const mytweet5 ={
+  status:'Would the #web exist today without #cpluscplus.....we need real #SoftwareEngineers here at Samvit'
+}
+
+const mytweet6 ={
   status:'Watch out for the cool new #Android app from our stable'  
+}
+
+var dayOfWeek = require('day-of-week').get
+console.log(dayOfWeek);
+
+const mytweet ={
+  status:'Checking in'  
 }
 
 T.post('statuses/update', mytweet, (err, response) => {
@@ -19,8 +50,8 @@ T.post('statuses/update', mytweet, (err, response) => {
         if(err){
            return console.log('Post failed : '+err[0].message);
         }
-      const username = response.user.name;
-      const followerscnt = response.followers_count;
+      const username = response.user.screen_name;
+      const followerscnt = response.friends_count;
       //const favoritedTweetId = response.id_str;
       console.log(`Your name:`+username);
       console.log(`Your follower count:`+followerscnt);
